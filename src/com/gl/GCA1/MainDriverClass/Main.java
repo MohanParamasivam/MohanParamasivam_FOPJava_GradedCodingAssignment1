0b5b5ccfe259a2d23e2bbdceae78a38cffa8cffa
MohanParamasivam <111678461+MohanParamasivam@users.noreply.github.com>
2022-10-08 15:20:59 +0530

    Add files via upload

---
+++ src/com/gl/GCA1/MainDriverClass/Main.java
@@ -0,0 +1,34 @@
+package com.gl.GCA1.MainDriverClass;
+
+import com.gl.GCA1.Departments.*;
+
+public class Main {
+	public static void main(String[] args) {
+
+		// Creating objects for various departments
+		HrDepartment hr = new HrDepartment();
+		TechDepartment tech = new TechDepartment();
+		AdminDepartment admin = new AdminDepartment();
+
+		// Invoking or calling methods of Admin department
+		System.out.println(" Welcome to " + admin.departmentName());
+		System.out.println(admin.getTodaysWork());
+		System.out.println(admin.getWorkDeadline());
+		System.out.println(admin.isTodayAHoliday() + '\n');
+
+		// Invoking or calling methods of HR department
+		System.out.println(" Welcome to " + hr.departmentName());
+		System.out.println(hr.doActivity());
+		System.out.println(hr.getTodaysWork());
+		System.out.println(hr.getWorkDeadline());
+		System.out.println(hr.isTodayAHoliday() + '\n');
+
+		// Invoking or calling methods of Tech department
+		System.out.println(" Welcome to " + tech.departmentName());
+		System.out.println(tech.getTodaysWork());
+		System.out.println(tech.getWorkDeadline());
+		System.out.println(tech.getTechStackInformation());
+		System.out.println(tech.isTodayAHoliday());
+	}
+
+}
